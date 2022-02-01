437dd088e2342acadc2bec70d203b062ee94b9ef
Anthony S <48108297+7kFour@users.noreply.github.com>
2022-02-01 15:49:21 -0600

    initial commit

---
+++ raylib-vcpkg-script.ps1
@@ -0,0 +1,18 @@
+# run this wherever you want it to be installed C:\Dev for instance
+# it will get vcpkg from github then install raylib through it 
+# saves a lot of setup time 
+
+git clone https://github.com/microsoft/vcpkg.git | Out-Null  
+
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg\vcpkg.exe integrate install
+
+# this is x86 by default
+#.\vcpkg\vcpkg.exe install raylib
+
+#if you want x64
+.\vcpkg\vcpkg install raylib:x64-windows
+
+
+
+
