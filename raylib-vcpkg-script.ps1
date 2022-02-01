28e1bc5a612979bbc9df5344eff771a501bff86a
Anthony S <48108297+7kFour@users.noreply.github.com>
2022-02-01 15:47:23 -0600

    Delete raylib-vcpkg-script.ps1

--- raylib-vcpkg-script.ps1
+++
@@ -1,8 +0,0 @@
-# copy and then run this script in whatever directory you make your raylib project in
-# it will get vcpkg from github then install raylib through it 
-# saves a lot of setup time 
-
-git clone https://github.com/microsoft/vcpkg.git | Out-Null  
-.\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg.exe install raylib
-.\vcpkg\vcpkg.exe integrate install
